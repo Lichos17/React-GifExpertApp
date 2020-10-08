637afccef893966a2eea7452d865bbf34f1d1eeb
Luis Zapata <zapataluis1@outlook.es>
2020-10-07 21:35:18 -0500

    Docs agregados

---
+++ docs/precache-manifest.20df8710ffade4f4565fcecc328d3769.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c0a1038e433fbfff40909b1f223c250",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f4a53cff0ef60e1ae626",
+    "url": "/static/css/main.eec95d37.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4a53cff0ef60e1ae626",
+    "url": "/static/js/main.25138347.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
